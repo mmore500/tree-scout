e5a513b16fd08a523995203558e4fde611c84370
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-04-02 16:20:14 -0700

    Seed global rngs for deterministic tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import random
+
+import numpy as np
 import pytest
 
 
@@ -9,3 +12,10 @@ def _session() -> None:
 @pytest.fixture(scope="module", autouse=True)
 def _module() -> None:
     pass
+
+
+@pytest.fixture(autouse=True)
+def reseed_random(request: pytest.FixtureRequest) -> None:
+    """Reseed random number generator to ensure determinstic test."""
+    random.seed(request.node.name)
+    np.random.seed(random.randint(1, 2**32))
